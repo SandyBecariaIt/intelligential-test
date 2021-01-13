fe52c692f4069b24a320839fc342f7cdbd1b29b2
Sandy Francisco <sandra.cortes@rappi.com>
2021-01-12 20:00:18 -0600

    spiral algorithm

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,67 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div v-for="(item, index) in item" :key="index">
+      <p>{{ item }}</p>
+    </div>
+    <p>Resultado: {{ seconArray }}</p>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  
+  data() {
+    return {
+      i: 0,
+      seconArray: [],
+      item: [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+      ]
+    }
+  },
+
+  mounted() {
+     this.getSidebar();
+  },
+
+  methods: {
+    getSidebar() {
+      var count = 0;
+      var colums = 0;
+      var rows = this.item.length;
+
+      this.getTop(count, colums, rows);
+      this.getRight(count, colums, rows);
+      this.getLeft(count, colums, rows);
+      this.getBottom(count, colums, rows);
+    },
+    getTop(count, colums, rows) {
+      for (count = colums; count < rows; count++) {
+        this.seconArray[this.i] = this.item[colums][count];
+        this.i ++;
+      }
+    },
+    getRight(count, colums, rows) {
+      for (count = colums + 1; count < rows; count++) {
+        this.seconArray[this.i] = this.item[count][rows-1];
+        this.i ++;
+      }
+    },
+    getLeft(count, colums, rows) {
+      for (count = rows - 2; count >= 0; count--) {
+        this.seconArray[this.i] = this.item[rows-1][count];
+        this.i ++;
+      }
+    },
+    getBottom(count, colums, rows) {
+      for (count = colums; count < rows-1 ; count ++) {
+        this.seconArray[this.i] = this.item[rows - 2][count];
+        this.i ++;
+      }
+    }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
